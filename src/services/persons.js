39a0da067bcd15d8b4fe60e11e987c592bee7546
Pedro Alcoba <pedralcg@gmail.com>
2025-06-01 22:58:36 +0200

    Part03d - Ejer 3.22 Lint configuration - Frontend

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,4 +1,4 @@
-import axios from 'axios'; // Importa axios para realizar peticiones HTTP
+import axios from 'axios' // Importa axios para realizar peticiones HTTP
 
 //// Define la URL base para el recurso 'persons' en tu json-server
 //// const baseUrl = 'http://localhost:3001/persons';
@@ -11,37 +11,37 @@ const baseUrl = '/api/persons'
 // Función para obtener todas las personas del servidor
 const getAll = () => {
   // Realiza una petición GET a la URL base
-  const request = axios.get(baseUrl);
+  const request = axios.get(baseUrl)
   // Devuelve la promesa que resolverá con los datos de la respuesta
-  return request.then(response => response.data);
-};
+  return request.then(response => response.data)
+}
 
 // Función para crear una nueva persona en el servidor
 const create = newObject => {
   // Realiza una petición POST a la URL base
   // Envía el 'newObject' (la nueva persona) en el cuerpo de la petición
-  const request = axios.post(baseUrl, newObject);
+  const request = axios.post(baseUrl, newObject)
   // Devuelve la promesa que resolverá con los datos de la respuesta (incluyendo el ID asignado por el servidor)
-  return request.then(response => response.data);
-};
+  return request.then(response => response.data)
+}
 
 // Función para eliminar una persona del servidor
 const remove = id => {
   // Realiza una petición DELETE a la URL específica del recurso (baseUrl + id)
-  const request = axios.delete(`${baseUrl}/${id}`);
+  const request = axios.delete(`${baseUrl}/${id}`)
   // Para las peticiones DELETE, a menudo no se necesita la data de la respuesta, solo confirmar que se completó.
   // axios.delete resuelve con el objeto de respuesta, no directamente con 'data' como GET/POST.
-  return request.then(response => response.data);
-};
+  return request.then(response => response.data)
+}
 
 // Función para actualizar una persona existente en el servidor
 const update = (id, newObject) => {
   // Realiza una petición PUT a la URL específica de la persona (baseUrl + id)
   // Envía el 'newObject' (la versión actualizada de la persona) en el cuerpo de la petición
-  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
   // Devuelve la promesa que resolverá con los datos de la respuesta
-  return request.then(response => response.data);
-};
+  return request.then(response => response.data)
+}
 
 // Exporta las funciones para que puedan ser utilizadas en otros módulos
 export default {
@@ -49,4 +49,4 @@ export default {
   create,
   remove,
   update
-};
+}
